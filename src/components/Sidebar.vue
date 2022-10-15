bb274259af76f47bb4b23ea776ed00bdc1e13437
appert <scoreur@gmail.com>
2022-10-15 10:48:39 +0200

    first commit

---
+++ src/components/Sidebar.vue
@@ -0,0 +1,46 @@
+<template>
+    <aside>
+        <div class="logo">
+            <img src="../assets/compass.png" alt="logo du site">
+        </div>
+        <div class="menu-toggle-wrap">
+            
+        </div>
+    </aside>
+
+</template>
+
+<script>
+</script>
+
+<style>
+
+</style>
+
+<style lang="scss" >
+aside {
+    display: flex;
+    flex-direction: column;
+    width: calc(2rem + 32px);
+    min-height: 100vh;
+    overflow: hidden;
+    padding: 1rem;
+
+    background-color: var(--dark);
+    color: var(--light);
+
+    transition: 0.2s ease-out;
+
+    .logo {
+        margin-bottom: 1rem;
+        img {
+            width: 2rem;
+        }
+    }
+
+    @media (max-width: 768px) {
+        position: fixed;
+        z-index: 99;
+    }
+}
+</style>
---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <template>
+  <main class="about-page">
+    <h1>About</h1>
+    <p>This is a about page</p>
+  </main>
+</template>
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    align-items: center;
+  }
+}
+</style>
